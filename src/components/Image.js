faf3a0f6d9703d8cd8eb25de7c5fb27fa25adfa5
platonian <prince@indshine.com>
2020-06-25 21:36:09 +0530

    Added margin

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = ({url, altDescription}) => {
     return (
             <img
-                className="py-2 w-1/3 align-middle object-cover"
+                className="p-2 w-1/3 align-middle object-cover"
                 src={url} alt={altDescription} />
     )
 }
