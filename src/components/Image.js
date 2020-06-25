5c783fa5805e2505efc61d0283b2beb9e68a9e08
platonian <prince@indshine.com>
2020-06-25 21:42:06 +0530

    Testing Padding change

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = ({url, altDescription}) => {
     return (
             <img
-                className="p-2 w-1/3 align-middle object-cover"
+                className="p-10 w-1/3 align-middle object-cover"
                 src={url} alt={altDescription} />
     )
 }
